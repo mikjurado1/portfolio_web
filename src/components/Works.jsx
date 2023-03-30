c1cf96afad0440bbec2d3cbeb677ddcc449ece8e
Michael Jurado <mikjurado1@gmail.com>
2023-03-30 14:54:30 +0200

    fin planeta

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,10 +1,12 @@
-import Tilt from 'react-tilt'
+
 import {motion} from 'framer-motion'
 import {styles} from '../styles'
 import {github} from '../assets'
 import {SectionWrapper} from '../constants/hoc'
 import {projects} from '../constants'
 import {fadeIn, textVariant} from '../utils/motion'
+import Tilt from 'react-parallax-tilt';
+
 
 const ProjectCard = ({index, name, description, tags, image, source_code_link})=>{
   return(
